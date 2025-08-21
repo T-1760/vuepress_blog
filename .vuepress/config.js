aae8832747346e41ec23c8dd46aa3d9ed5f5e40f
T-1760 <480085735@qq.com>
2025-08-22 00:02:53 +0800

    线上资源前缀修复

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,7 @@ const head = require("./configs/head")
 const themeConfig = require("./configs/themeConfig")
 
 module.exports = {
+  base: '/vuepress_blog/',
   title: '无常记', // 博客标题
   description: 'Great Frontend 喜常于记',
   head,
